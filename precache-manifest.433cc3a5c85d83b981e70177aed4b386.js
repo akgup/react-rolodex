87989c20c563e937357c26fdb3c458f78ee3e98c
akgup <akshay.gupta@vuclip.com>
2020-04-14 16:12:22 +0530

    Updates

---
+++ precache-manifest.433cc3a5c85d83b981e70177aed4b386.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fad3858e94f76cde4ea2d24660ad59b",
+    "url": "/react-rolodex/index.html"
+  },
+  {
+    "revision": "b32795182e3ffe789b88",
+    "url": "/react-rolodex/static/css/main.48f09ec7.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/react-rolodex/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-rolodex/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b32795182e3ffe789b88",
+    "url": "/react-rolodex/static/js/main.91624834.chunk.js"
+  },
+  {
+    "revision": "bff0e6aef3ce725fc2ff",
+    "url": "/react-rolodex/static/js/runtime-main.99be1617.js"
+  }
+]);
